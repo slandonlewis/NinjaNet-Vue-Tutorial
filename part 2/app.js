e07c28cfcd2a40feb41efade71b74bec8bac57e3
Seth Lewis <slandonlewis@gmail.com>
2024-02-21 09:57:51 -0600

    implemented more event listeners

--- part 2/app.js
+++ part 2/app.js
@@ -2,9 +2,12 @@ const app = Vue.createApp({
     // data function can store things like variables to be used later
     data() {
         return {
-            showText: true,
+            showText: false,
+            showBoxes: false,
             title: 'Changable Text',
-            number: 0
+            number: 0,
+            x: 0,
+            y: 0
         }
     },
     // functions
@@ -14,6 +17,19 @@ const app = Vue.createApp({
         },
         toggleShowText() {
             this.showText = !this.showText // change showText to whatever it currently is NOT set to
+        },
+        toggleShowBoxes() {
+            this.showBoxes = !this.showBoxes
+        },
+        handleEvent(e, data) {
+            window.alert(e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })

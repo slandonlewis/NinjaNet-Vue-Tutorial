012492b4c38debddbe733cd8ff4731ef8e45c260
Seth Lewis <slandonlewis@gmail.com>
2024-02-21 11:51:50 -0600

    implemented dynamic classes

--- part 2 - mouse events, v-for, attribute binding, dynamic classes, computed properties/app.js
+++ part 2 - mouse events, v-for, attribute binding, dynamic classes, computed properties/app.js
@@ -10,9 +10,9 @@ const app = Vue.createApp({
             x: 0,
             y: 0,
             animeShows: [
-                { title: 'FullMetal Alchemist Brotherhood', studio: 'Studio BONES', imgURL: 'https://upload.wikimedia.org/wikipedia/en/7/7e/Fullmetal_Alchemist_Brotherhood_key_visual.png'},
-                { title: 'Dragon Ball Z', studio: 'Toei Animation', imgURL: 'https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/35e4ac6339f5fdcc164160a5755790cd.jpe'},
-                { title: 'Jujutsu Kaisen', studio: 'Studio MAPPA', imgURL: 'https://resized-images.crazylister.com/68747470733a2f2f696d672e6372617a796c69737465722e636f6d2f37393834342f696d616765732f4a756a757473752532302d253230536d616c6c2e6a70673f76657273696f6e49643d62706f315570304a754f37677a497639447a55386e765a47504f793332657771/auto/502.jpg'}
+                { title: 'FullMetal Alchemist Brotherhood', studio: 'Studio BONES', ongoing: false, imgURL: 'https://upload.wikimedia.org/wikipedia/en/7/7e/Fullmetal_Alchemist_Brotherhood_key_visual.png'},
+                { title: 'Dragon Ball Z', studio: 'Toei Animation', ongoing: false, imgURL: 'https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/35e4ac6339f5fdcc164160a5755790cd.jpe'},
+                { title: 'Jujutsu Kaisen', studio: 'Studio MAPPA', ongoing: true, imgURL: 'https://resized-images.crazylister.com/68747470733a2f2f696d672e6372617a796c69737465722e636f6d2f37393834342f696d616765732f4a756a757473752532302d253230536d616c6c2e6a70673f76657273696f6e49643d62706f315570304a754f37677a497639447a55386e765a47504f793332657771/auto/502.jpg'}
             ],
             urlToBind: "https://www.youtube.com"
         }
